02f318d89be6c8732fef45afc5d0b28a81d23af1
donatomontrone <donato.montrone@outlook.com>
2023-05-05 11:27:49 +0200

    added Snack2

---
+++ src/org/lessons/java/Snack2.java
@@ -0,0 +1,40 @@
+package org.lessons.java;
+
+import java.util.Random;
+
+public class Snack2 {
+	public static void main(String[] args) {
+		String[] names = {
+				"Donato",
+				"Marco",
+				"Filippo",
+				"Antonio",
+				"Giada",
+				"Giovanna",
+				"Alessia",
+				"Gabriella"
+		};
+		String[] surnames = {
+				"Disit",
+				"Rossi",
+				"Verdi",
+				"Scari",
+				"Giada",
+				"Lassi",
+				"Cappesi",
+				"Notti"
+		};
+		
+		int namesLnt = names.length;
+		
+		for (int i = 0; i < namesLnt; i++) {
+			Random rnd = new Random();
+			int val1 = rnd.nextInt(namesLnt);
+			int val2 = rnd.nextInt(namesLnt);
+			
+			String newPerson = names[val1] + " " + surnames[val2];
+			
+			System.out.println(newPerson);
+		}
+	}
+}
