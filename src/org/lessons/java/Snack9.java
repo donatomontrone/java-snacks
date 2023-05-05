f286582fecfb34d2a574b8dd9eb028b388eeca2a
donatomontrone <donato.montrone@outlook.com>
2023-05-05 15:14:50 +0200

    addeed snack9

--- src/org/lessons/java/Snack9.java
+++ src/org/lessons/java/Snack9.java
@@ -1,5 +1,20 @@
 package org.lessons.java;
 
-public class Snack9 {
+import java.util.Scanner;
 
+public class Snack9 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Inserisci la base del rettangolo");
+		int base = in.nextInt();
+		System.out.println("Inserisci l'altezza del rettangolo");
+		int height = in.nextInt();
+		in.close();
+		
+		int perimeter = (base + height) * 2 ;
+		int area = base * height;
+		
+		System.out.println("Perimetro = " + perimeter);
+		System.out.println("Area = " + area);
+	}
 }
