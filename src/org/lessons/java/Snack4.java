5b917eb555644012f309539b3ec58950d0fb2919
donatomontrone <donato.montrone@outlook.com>
2023-05-05 11:44:20 +0200

    added snack4

---
+++ src/org/lessons/java/Snack4.java
@@ -0,0 +1,25 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class Snack4 {
+	public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	
+	System.out.println("Inserisci una parola");
+	String word = in.nextLine();
+	in.close();
+	
+	int wordLnt = word.length();
+	String reverse = "";
+	for (int i = wordLnt - 1; i >= 0; i--) {
+		reverse += word.charAt(i);
+	}
+	
+	System.out.println(
+			reverse.equals(word)
+			? "Parola palindorma" 
+			: "Parola non palindroma"
+				);
+	}
+}
