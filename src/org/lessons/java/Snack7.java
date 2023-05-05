c95205749a3c73828fcedf5f18ec7407bc2ff0e3
donatomontrone <donato.montrone@outlook.com>
2023-05-05 16:05:11 +0200

    fix snack7

--- src/org/lessons/java/Snack7.java
+++ src/org/lessons/java/Snack7.java
@@ -1,12 +1,17 @@
 package org.lessons.java;
 
+import java.util.Random;
+
 public class Snack7 {
 	public static void main(String[] args) {
-		int x = 0;
-
-		do {
-			x++;
-			System.out.println(x);
-		} while (!(x % 3 == 0 && x % 5 == 0));
+		Random rnd = new Random();
+		int rndVal = rnd.nextInt(101);
+		while(rndVal % 3 != 0 || rndVal % 5 != 0) {
+			
+			System.out.println(rndVal);
+			rndVal = rnd.nextInt(101);
+		}
+		System.out.println(rndVal);
 	}
+	
 }
