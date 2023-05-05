3e27cda54dc7a0a2cf1bd770961b42ddf581a050
donatomontrone <donato.montrone@outlook.com>
2023-05-05 10:40:45 +0200

    added snack1

--- src/org/lessons/java/Snack1.java
+++ src/org/lessons/java/Snack1.java
@@ -1,5 +1,23 @@
 package org.lessons.java;
 
-public class Snack1 {
+import java.util.Scanner;
 
+public class Snack1 {
+public static void main(String[] args) {
+	
+	Scanner in = new Scanner(System.in);
+	
+		System.out.println("Inserisci un numero");
+		
+			int val = in.nextInt();
+		
+	in.close();
+	
+	System.out.println(
+			val % 2 == 0 
+			? val
+			: val + 1
+			);
+}
+	
 }
